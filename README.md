6ebb329c7138547ea8d196cdaceafa6f98ab8706
miguel reyes <miguel.paprofleci@gmail.com>
2025-03-27 18:25:50 -0600

    hola xd

---
+++ README.md
@@ -0,0 +1,7 @@
+# PWCI
+
+To start a local server go to the root folder and execute 
+
+```
+php -S localhost:8000 -t public
+```
---
+++ views/notes/show.view.php
@@ -0,0 +1,17 @@
+<?php require base_path('views/partials/head.php') ?>
+<?php require base_path('views/partials/nav.php') ?>
+<?php require base_path('views/partials/banner.php') ?>
+    <main>
+        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+            <p class="mb-6">
+                <a href="/notes"
+                   class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Go
+                    Back</a>
+            </p>
+            <p>
+                <?= htmlspecialchars($note['note']) ?>
+            </p>
+
+        </div>
+    </main>
+<?php require base_path('views/partials/footer.php') ?>
